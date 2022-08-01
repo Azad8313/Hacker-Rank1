09d10103e8239e74588054fcfabfdd02ce854986
Azad8313 <110417443+Azad8313@users.noreply.github.com>
2022-08-01 23:20:26 +0530

    Add files via upload

---
+++ Task3(Math)/Find Angle MBC.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+ab=int(input())
+bc=int(input())
+print(round(math.degrees(math.atan(ab/bc))),chr(176),sep='')
---
+++ Task3(Math)/Polar Coordinates.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+z=complex(input())
+r,phi=cmath.polar(z)
+print(r,phi,sep='\n')
